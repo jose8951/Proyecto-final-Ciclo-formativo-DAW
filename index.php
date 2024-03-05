3776f61149fb03eabefa10ec7a840cbc88237029
jose vega <josves222@gmail.com>
2024-03-05 12:20:44 +0100

    Add files via upload
    Proyecto-final-Ciclo-formativo-DAW

---
+++ index.php
@@ -0,0 +1,5 @@
+<!-- Página principal del proyecto index.php
+Proyecto Academia Omega  del ciclo formativo de grado superior de desarrollo de aplicaciones web 
+Por José Alfonso Vega  Muñoz   -->
+<?php
+include_once 'vista/login.php';
